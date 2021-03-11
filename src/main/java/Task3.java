f22e3efca14ef81f0ca51e3bdeb16283537f6241
Eugene Burdukov <74274237+eugeneburdukov@users.noreply.github.com>
2021-03-11 17:48:13 +0200

    commit

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -6,12 +6,12 @@ public class Task3 {
 			1) функции y = 2*sin(x)-4; в диапазоне [-5, 5] c шагом 0.1;
 			2) функции y = 2*tg(x)-4*sin(2*x); в диапазоне [-1, 1] c шагом 0.01;
      */
-
-        // [-7, 20]
+        
         int min = -5;
         int max = 5;
 
-        for (int x = min; x <= max; x++) {
+        for (double x = min; x <= max; x++) {
+            x = x + 0.1;
             double y = 2 * Math.sin(x) - 4;
             System.out.println("Y = " + y + "; X =" + x + ";");
         }
